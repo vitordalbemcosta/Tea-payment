93ad0e086528e17b93567a2fe26159a146274b1a
Vitor Dalbem Costa <dalbemcosta@gmail.com>
2022-07-14 13:21:06 -0500

    will start components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
+import React, {useState} from 'react'
 import './App.css';
+import axios from "axios"
+
 
 function App() {
+
+  
+
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <div className="container">
+        <div className="top">
+          <div className="day">
+            <h3>Monday</h3>
+          </div>
+          <div className="weather-icon">
+            <p>Some Icon</p>
+          </div>
+          <div className="temperature">
+            <p>25c</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="min">
+            <p>min</p>
+            <p>22c</p>
+          </div>
+          <div className="max">
+            <p>max</p>
+            <p>27c</p>
+          </div>
+          <div className="wind-speed">
+            <p>wind speed</p>
+            <p>27c</p>
+          </div>
+          <div className="wind-direction">
+            <p>Wind dir</p>
+            <p>SW</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Days.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Days() {
+  return (
+    <div>Days</div>
+  )
+}
+
+export default Days
