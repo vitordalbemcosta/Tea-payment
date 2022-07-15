ccdf6eb7e2e1540f34b551cda0f5eddb1cf39191
Vitor Dalbem Costa <dalbemcosta@gmail.com>
2022-07-14 21:04:44 -0500

    api not working so far

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 
+// nao estou conseguindo fazer a api funcionar
+
 export const getWeather = async (name) => {
     const response = await axios.get(
       `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=336ce4dce3a588920b3cea8fb00882d6`
